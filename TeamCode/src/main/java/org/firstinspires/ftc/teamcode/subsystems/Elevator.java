e099c544e1b033b23671aa3f006c9929366c1bd3
AJBellGilmour <113713587+AJBellGilmour@users.noreply.github.com>
2022-11-29 11:58:33 -0500

    Start building out elevator class
    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.wrappers.SparkMINIMotor;
+
 public class Elevator {
+    private SparkMINIMotor leftArmControl;
+    private SparkMINIMotor rightArmControl;
+    private Telemetry telemetry;
+    public Elevator (HardwareMap hardwareMap, Telemetry telemetry){
+        leftArmControl = new SparkMINIMotor(hardwareMap, "left_arm_control", "left_arm_control_encoder");
+        rightArmControl = new SparkMINIMotor(hardwareMap, "right_Arm_Control", "right_Arm_Control_Encoder");
+    }
+
 }
