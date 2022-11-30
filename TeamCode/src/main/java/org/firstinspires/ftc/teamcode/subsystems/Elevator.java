af696e451aad5c3017ce02be6b6f8c513c56eee4
AJBellGilmour <113713587+AJBellGilmour@users.noreply.github.com>
2022-11-30 13:44:25 -0500

    new elevator fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -4,28 +4,36 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.wrappers.SparkMINIMotor;
 
 public class Elevator {
     private SparkMINIMotor leftArmControl;
     private SparkMINIMotor rightArmControl;
     private Telemetry telemetry;
+
     public Elevator (HardwareMap hardwareMap, Telemetry telemetry){
+        this.telemetry = telemetry;
+
         leftArmControl = new SparkMINIMotor(hardwareMap, "left_arm_control", "left_arm_control_encoder");
         rightArmControl = new SparkMINIMotor(hardwareMap, "right_Arm_Control", "right_Arm_Control_Encoder");
     }
     //This stuff is just unfinished
     // Feel free to finish
-public void raiseElevator(){
-    rightArmControl.setPower();
-    rightArmControl.set
-    leftArmControl.setPower();
-    leftArmControl.set
-}
-public void lowerElevator(){
-        rightArmControl.setPower();
-        rightArmControl.setMode()
-        leftArmControl.setPower();
-        leftArmControl.set
-public void stopMotors
+    public void raiseElevator(){
+        rightArmControl.setPower(0.5);
+        rightArmControl.setTargetPosition(Constants.CLAW_OPENED_POSITION);
+        leftArmControl.setPower(0.5);
+        leftArmControl.setTargetPosition(Constants.CLAW_OPENED_POSITION);
+    }
+    public void lowerElevator() {
+        rightArmControl.setPower(-0.5);
+        rightArmControl.setTargetPosition(Constants.CLAW_OPENED_POSITION);
+        leftArmControl.setPower(-0.5);
+        leftArmControl.setTargetPosition(Constants.CLAW_OPENED_POSITION);
+    }
+    public void stopMotors() {
+            rightArmControl.setPower(0.0f);
+            leftArmControl.setPower(0.0f);
+    }
 }
